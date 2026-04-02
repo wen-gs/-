89eacaf0d0af2b79d5f7cbcea8a64aede9858db0
wen-gs <gaoshengwen876@gmail.com>
2026-04-02 18:44:21 +0800

    build: compile static site to docs/ for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/-/',
   root: '.',
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     emptyOutDir: true,
   },
 });
